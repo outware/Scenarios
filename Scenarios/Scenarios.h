881d8d5d883701aa98eca03ea84cc02419055e9e
Adam Sharp <adsharp@me.com>
2015-10-26 16:21:34 +1100

    Add an Xcode project

---
+++ Scenarios/Scenarios.h
@@ -0,0 +1,19 @@
+//
+//  Scenarios.h
+//  Scenarios
+//
+//  Created by Adam Sharp on 26/10/2015.
+//  Copyright © 2015 Outware Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scenarios.
+FOUNDATION_EXPORT double ScenariosVersionNumber;
+
+//! Project version string for Scenarios.
+FOUNDATION_EXPORT const unsigned char ScenariosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scenarios/PublicHeader.h>
+
+
