ef52cc02c959bf64e8f7c418ec0f2c04d8ed0242
Gavan Chan <gavan.chan@outware.com.au>
2017-05-26 14:42:53 +1000

    Document the 'Prepared' stage of building a scenario

--- Scenarios/Components/Types.swift
+++ Scenarios/Components/Types.swift
@@ -1,5 +1,25 @@
 //  Copyright © 2017 Outware Mobile. All rights reserved.
 
+/// A scenario that has had its preconditions prepared, and is ready to have its
+/// state acted upon, or to have state that is a result of preparing its _preconditions_
+/// asserted.
+///
+/// - note: A scenario is generally `Prepared` as a result of a `Given` step, followed
+///         by zero or more `And` steps.
+///
+/// e.g.
+///
+/// - __Given__ the user is on the Home Screen
+/// - __And__ the currently logged-in account is a Type A account
+/// - ...
+///
+/// This scenario performs steps to take the user to the home screen and manipulates
+/// setup information such that their logged-in account is a "Type A" account.
+///
+/// At this stage, the scenario is `Prepared` to:
+/// - have some state on the screen (such as the title of the screen) be asserted.
+/// - be manipulated / acted upon (for example, by tapping on a button, or scrolling down
+///     a list), in preparation for future state assertion.
 public final class Prepared: Assertable, Actionable, Extendable {
 
   private var builder: ScenarioBuilder
